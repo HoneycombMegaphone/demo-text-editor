2633c47059e7afa93c40363bb7ea8d226975ff3a
MegaHakkero <runezhevitz@gmail.com>
2025-02-16 21:16:48 +0200

    fix buffer init

--- buffer.c
+++ buffer.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "buffer.h"
 
-int main() {
-	return 0;
+GapBuffer gbinit() {
+	GapBuffer buf;
+	buf.bufferSize = BUFFERBLOCKSIZE;
+	buf.buffer = malloc(buf.bufferSize);
+	buf.cursor = buf.buffer;
+	buf.cursorEnd = buf.cursor; // no characters at the start...
+	buf.cursorLeft = buf.buffer + buf.bufferSize; // and no characters at the end
+	
+	return buf;
 }
